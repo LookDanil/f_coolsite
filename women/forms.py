c592ba277a2d18e6a834fd09ef13355bcab070d3
LookDanil <Nikonorov_17@mail.ru>
2023-09-04 14:57:05 +0300

    Добавил авторищацию пользователей

--- women/forms.py
+++ women/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 from .models import *
@@ -12,8 +14,20 @@ class AddPostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form_input'}),
             'content': forms.Textarea(attrs={'cols': 60, 'rows': 10})
         }
+
     def clean_title(self):
         title = self.cleaned_data['title']
-        if len(title)>200:
+        if len(title) > 200:
             raise ValidationError('Длина заголовка слишком большая!')
         return title
+
+
+class AddUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form_input'}))
+    email = forms.EmailField(label='Почта', widget=forms.EmailInput(attrs={'class': 'form_input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form_input'}))
+    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput(attrs={'class': 'form_input'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- women/views.py
+++ women/views.py
@@ -1,9 +1,10 @@
+
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DeleteView, CreateView
 
-from .forms import AddPostForm
+from .forms import AddPostForm, AddUserForm
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
@@ -75,7 +76,18 @@ class WomenCategory(DataMixin, ListView):
         c_dic = self.get_user_context(title="Категория - " + str(context['posts'][0].cat_slug),
                                       cat_selected=self.kwargs[
                                           'cat_slug_id'])
-        return dict(list(context.items())+list(c_dic.items()))
+        return dict(list(context.items()) + list(c_dic.items()))
 
     def get_queryset(self):
         return Women.objects.filter(cat_slug_id=self.kwargs['cat_slug_id'], is_published=True)
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = AddUserForm
+    template_name = "women/register.html"
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_dic = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_dic.items()))
